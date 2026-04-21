bb0879571175a3e0a8b466c8c34dcf052357cb32
Gigio-Bagigio <dk.dhaouadi.karim@gmail.com>
2026-04-21 13:55:41 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# towerDefenseTpsit
+# Space Defense
+Modalità di gioco:
+* R -> Attiva raggio attraente
+* Space -> Lancia impulso
+* C -> Campo magnetco
--- src/main/java/com/example/dino/Impulsi.java
+++ src/main/java/com/example/dino/Impulsi.java
@@ -1,4 +1,6 @@
 package com.example.dino;
 
 public class Impulsi {
+    // ci sarà una lista di impulsi, ogni impulso ha la propria direzione/traiettoria, questa classe deve gestire vita e morte degli impulsi
+
 }
