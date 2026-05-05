7ea693a16ffcfc7f85489ca63690d06de9afde8c
marcllevo <marco.levorato@cz.lan>
2026-05-05 13:26:54 +0200

    modificato

--- src/main/java/com/example/dino/InterfacciaGiocatore.java
+++ src/main/java/com/example/dino/InterfacciaGiocatore.java
@@ -10,21 +10,20 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.MinimalHTMLWriter;
+
 public class InterfacciaGiocatore extends Application {
 
     @Override
     public void start(Stage stage) {
 
-
-
-
-
         Canvas canvas = new Canvas(1920, 1080);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-
         StackPane root = new StackPane(canvas);
+        Ostacolo ostacolo = new Ostacolo(200, 200, 3, 2);
+        Image img = new Image(getClass().getResourceAsStream("/assets/spaceBackground.png"));
+        Image img2 = new Image(getClass().getResourceAsStream("/assets/Asteroids_Foreground.png"));
 
-        Image img = new Image(getClass().getResourceAsStream("/assets/6GujW+.png"));
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
@@ -32,6 +31,9 @@ public class InterfacciaGiocatore extends Application {
 
                 gc.fillRect(0, 0, 1920, 1080); // Cancella tutto
                 gc.drawImage(img, 0 ,0 ,1920, 1080);
+                ostacolo.update();
+                ostacolo.draw(gc);
+
             }
         };
         timer.start();
